6c9700e321f763b9a8e8a7127340bc28e20a2752
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-03-04 17:11:12 +0900

    Update.

--- Program.cs
+++ Program.cs
@@ -1,32 +1,82 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
 
-namespace ConsoleAppG
+namespace himekuri_shaper
 {
-    static class Program
+    public static class Himekuri
     {
-        static void Main()
+        public static void Main(string[] args)
         {
-            // Method
-            DateTime dt = DateTime.Now;
-            string str = "令和";
-            string str2 = "R0";
-            string ver = "1.0.0";
+            try
+            {
+                // 宣言
+                var dt = DateTime.Now;
+                const string str = "令和";
+                const string str2 = "R0";
 
-            // Calc
-            var nengo = (dt.Year + "年" + dt.Month + "月" + dt.Day + "日" + " : " + dt.Hour + "時" + dt.Minute + "分" + dt.Second + "秒");
-            var reiwa = (str + (dt.Year - 2018) + "年"  + dt.Month + "月" + dt.Day + "日");
-            var reiwa2 = (str2 + (dt.Year - 2018) + "." + dt.Month + "." + dt.Day);
+                // 計算
+                var reiwa = (str + (dt.Year - 2018) + "年" + dt.Month + "月" + dt.Day + "日");
+                var reiwa2 = (str2 + (dt.Year - 2018) + "." + dt.Month + "." + dt.Day);
 
-            // Count 
-            var count = new DateTime(dt.Year , dt.Month, dt.Day);
-            var count2 = new DateTime(dt.Year + 1,1,1);
-            double oshogatsu = (count2 - count).TotalDays;
+                // 入れ子
+                var count = new DateTime(dt.Year, dt.Month, dt.Day);
+                var count2 = new DateTime(dt.Year + 1, 1, 1);
+                var gantan = (count2 - count).TotalDays;
 
-            // Console
-            Console.WriteLine(nengo);
-            Console.WriteLine(reiwa + " : " +  reiwa2);
-            Console.WriteLine("来年の1月1日まであと：" +(oshogatsu - 1) + "日です。");
-            Console.WriteLine("日めくりの数え番号：" + ver);
+                // 参照先
+                var nengo = (dt.Year + "年" + dt.Month + "月" + dt.Day + "日" + " : " + dt.Hour + "時" + dt.Minute + "分" +
+                             dt.Second + "秒" + " : " + dt.ToString("dddd"));
+                var hagoita = reiwa + " : " + reiwa2;
+                var oshogatsu = "来年の1月1日まであと：" + (gantan - 1) + "日です。";
+
+                // 安定ソート
+                var takoage = new[]
+                {
+                    oshogatsu,
+                    hagoita,
+                    nengo
+                };
+
+                var orderByList = takoage.OrderBy(x => x);
+                foreach (var x in orderByList )
+                {
+                    Console.WriteLine(x);
+                }
+            }
+
+            catch (Exception e)
+            {
+                // 出力
+                Trace.WriteLine(e.Message);
+            }
+            finally
+            {
+                try
+                {
+                    // 宣言
+                    const string ver = "1.0.1";
+                    const string himekuriShaperVersion = "日めくりの数え番号：" + ver;
+
+                    // 安定ソート
+                    var versioner = new[]
+                    {
+                        himekuriShaperVersion
+                    };
+
+                    var orderList = versioner.OrderBy(y => y);
+
+                    foreach (var y in orderList)
+                    {
+                        Console.WriteLine(y);
+                        Console.ReadKey();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Trace.WriteLine(ex.Message);
+                }
+            }
         }
     }
 }
